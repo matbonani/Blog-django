1575f5544c96bc83ecb154d0f0858805c7e85321
Matheus Bonani <bonani.matheus@gmail.com>
2022-03-23 21:03:41 -0300

    Adicionando post na pagina

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import IndexView, PostDetailView
+from .views import IndexView, PostDetailView, AddPostView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('artigo/<int:pk>', PostDetailView.as_view(), name='post_detail')
+    path('artigo/<int:pk>', PostDetailView.as_view(), name='post_detail'),
+    path('post/', AddPostView.as_view(), name='add_post'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
 
 from .models import Post
 
@@ -11,3 +12,10 @@ class IndexView(ListView):
 class PostDetailView(DetailView):
     model = Post
     template_name = 'artigo_detail.html'
+
+
+class AddPostView(CreateView):
+    model = Post
+    template_name = 'add_post.html'
+    fields = ['autor', 'titulo', 'body']
+    success_url = reverse_lazy('index')
