d35c8e8476a2d52b3bfef787483cc531e4f0ba3e
Matheus Bonani <bonani.matheus@gmail.com>
2022-03-31 17:33:28 -0300

    adicionando sistema de alteração de cadastro

--- membros/urls.py
+++ membros/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
+from . import views
 
-from .views import UserRegisterView
+from .views import UserRegisterView, UserEditView, PasswordsChangeView
 
 urlpatterns = [
     path('registro/', UserRegisterView.as_view(), name='registro'),
+    path('edit_profile/', UserEditView.as_view(), name='edit_profile'),
+    path('password/', PasswordsChangeView.as_view()),
+    path('password_sucess', views.password_sucess, name='pasword_sucess')
 
 ]
--- membros/views.py
+++ membros/views.py
@@ -1,7 +1,9 @@
 from django.views import generic
 from django.urls import reverse_lazy
+from django.contrib.auth.views import PasswordChangeView
+from django.shortcuts import render
 
-from .forms import SignUpForm
+from .forms import SignUpForm, EditProfileForm, PasswordChangingForm
 
 
 class UserRegisterView(generic.CreateView):
@@ -9,3 +11,23 @@ class UserRegisterView(generic.CreateView):
     template_name = 'registration/registro.html'
     success_url = reverse_lazy('login')
 
+
+class UserEditView(generic.UpdateView):
+    form_class = EditProfileForm
+    template_name = 'registration/edit_profile.html'
+    success_url = reverse_lazy('index')
+
+    def get_object(self):
+        return self.request.user
+
+
+class PasswordsChangeView(PasswordChangeView):
+    form_class = PasswordChangingForm
+    # form_class = PasswordChangeForm
+    # success_url = reverse_lazy('index')
+    success_url = reverse_lazy('pasword_sucess')
+    template_name = 'registration/change-password.html'
+
+
+def password_sucess(request):
+    return render(request, 'registration/password_sucess.html', {})
