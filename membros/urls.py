44f45b6bb30f461a5738ebc0c305e097a85b0048
Matheus Bonani <bonani.matheus@gmail.com>
2022-04-05 19:53:51 -0300

    adicionando template/view/url edit_profile_page

--- membros/urls.py
+++ membros/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-from .views import UserRegisterView, UserEditView, PasswordsChangeView, ShowProfilePageView
+from .views import UserRegisterView, UserEditView, PasswordsChangeView, ShowProfilePageView, EditProfilePageView
 
 urlpatterns = [
     path('registro/', UserRegisterView.as_view(), name='registro'),
@@ -9,5 +9,6 @@ urlpatterns = [
     path('password/', PasswordsChangeView.as_view()),
     path('password_sucess', views.password_sucess, name='pasword_sucess'),
     path('<int:pk>/profile/', ShowProfilePageView.as_view(), name='show_profile_page'),
+    path('<int:pk>/edit_profile_page/', EditProfilePageView.as_view(), name='edit_profile_page'),
 
 ]
--- membros/views.py
+++ membros/views.py
@@ -44,3 +44,10 @@ class ShowProfilePageView(DetailView):
         page_user = get_object_or_404(Profile, id=self.kwargs['pk'])
         context['page_user'] = page_user
         return context
+
+
+class EditProfilePageView(generic.UpdateView):
+    model = Profile
+    template_name = 'registration/edit_profile_page.html'
+    fields = ['bio', 'profile_pic', 'facebook', 'twitter', 'instagram', 'linkedin']
+    success_url = reverse_lazy('index')
