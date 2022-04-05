4fa542c2b2cb2d4aef1ec5b2a51b1938af5c5fb1
Matheus Bonani <bonani.matheus@gmail.com>
2022-04-05 18:14:43 -0300

    adicionando template/view/url do perfil

--- membros/urls.py
+++ membros/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from . import views
 
-from .views import UserRegisterView, UserEditView, PasswordsChangeView
+from .views import UserRegisterView, UserEditView, PasswordsChangeView, ShowProfilePageView
 
 urlpatterns = [
     path('registro/', UserRegisterView.as_view(), name='registro'),
     path('edit_profile/', UserEditView.as_view(), name='edit_profile'),
     path('password/', PasswordsChangeView.as_view()),
-    path('password_sucess', views.password_sucess, name='pasword_sucess')
+    path('password_sucess', views.password_sucess, name='pasword_sucess'),
+    path('<int:pk>/profile/', ShowProfilePageView.as_view(), name='show_profile_page'),
 
 ]
--- membros/views.py
+++ membros/views.py
@@ -1,9 +1,11 @@
 from django.views import generic
 from django.urls import reverse_lazy
 from django.contrib.auth.views import PasswordChangeView
-from django.shortcuts import render
+from django.views.generic import DetailView
+from django.shortcuts import render, get_object_or_404
 
 from .forms import SignUpForm, EditProfileForm, PasswordChangingForm
+from core.models import Profile
 
 
 class UserRegisterView(generic.CreateView):
@@ -31,3 +33,14 @@ class PasswordsChangeView(PasswordChangeView):
 
 def password_sucess(request):
     return render(request, 'registration/password_sucess.html', {})
+
+
+class ShowProfilePageView(DetailView):
+    model = Profile
+    template_name = 'registration/user_profile.html'
+
+    def get_context_data(self, *args,  **kwargs):
+        context = super(ShowProfilePageView, self).get_context_data(*args, **kwargs)
+        page_user = get_object_or_404(Profile, id=self.kwargs['pk'])
+        context['page_user'] = page_user
+        return context
